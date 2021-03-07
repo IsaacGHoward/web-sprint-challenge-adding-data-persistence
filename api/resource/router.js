0e9f97812e8faaad0ad04c2962ef1197b119df9c
IsaacGHoward <howardisaac100@gmail.com>
2021-03-06 19:55:25 -0800

    Adding / Getting resources

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,21 @@
 // build your `/api/resources` router here
+const express = require('express')
+const Resources = require('./model.js')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  Resources.getResources()
+    .then(resources => {
+      res.json(resources)
+    })
+})
+
+router.post('/', (req,res) => {
+  Resources.addResource(req.body)
+    .then(resource => {
+      res.json(resource);
+    })
+});
+
+module.exports = router;
