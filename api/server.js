a355c7e9f03f5d2b70b08ed6e4a5abf35ecfb7e0
IsaacGHoward <howardisaac100@gmail.com>
2021-03-06 20:41:50 -0800

    Get / post tasks

--- api/server.js
+++ api/server.js
@@ -3,11 +3,13 @@ const express = require('express');
 
 const ResourcesReouter = require('./resource/router');
 const ProjectsRouter = require('./project/router');
+const TasksRouter = require('./task/router');
 
 const server = express();
 
 server.use(express.json());
 server.use('/api/resources', ResourcesReouter);
 server.use('/api/projects', ProjectsRouter);
+server.use('/api/tasks', TasksRouter);
 
 module.exports = server;
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,25 @@
 // build your `/api/tasks` router here
+const express = require('express')
+const Tasks = require('./model.js')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  Tasks.getTasks()
+    .then(tasks => {
+      tasks.forEach(task => {
+        task.task_completed === 1 ? task.task_completed = true : task.task_completed = false;
+      });
+      res.json(tasks)
+    })
+})
+
+router.post('/', (req,res) => {
+  Tasks.addTask(req.body)
+    .then(task => {
+      task.task_completed === 1 ? task.task_completed = true : task.task_completed = false;
+      res.json(task);
+    })
+});
+
+module.exports = router;
